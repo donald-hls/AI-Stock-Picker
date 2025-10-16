245fd1ca7f43af6ebb069cd8d640182387d8369e
donaldlihe@gmail.com <donaldlihe@gmail.com>
2025-10-15 21:50:50 -0400

    added helper - limiting extreme vals

--- utility.py
+++ utility.py
@@ -15,3 +15,17 @@ def ratio_calculation(S1, S2, eps = 1e-9):
     denominator = S2.mask(S2.abs() < eps, np.nan)
     ratio = S1.divide(denominator)
     return ratio.where(np.isfinite(ratio))
+
+def limiting_extreme_values(s, p=0.01):
+    """
+    this is the process of windsorizing the data to remove any extreme values.
+    limiting_extreme_values(s, p=0.01): Series, float -> Series
+    """
+    # if not enough data, return the original series
+    if s.notna().sum() < 15: 
+        return s
+    lower_quantile = s.quantile(p)
+    upper_quantile = s.quantile(1-p)
+    # Assigns values outside boundary to boundary values. (to trim inputs)
+    return s.clip(lower_quantile, upper_quantile)
+    
